6056ebf3bd56e56e4580f032ad03c9a862001198
Sharan S <sevis1945@gmail.com>
2026-04-08 20:06:33 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,17 @@
+// odd or even
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+}
+    
